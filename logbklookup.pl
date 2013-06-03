ad6251b6690ccd986c64bd67d39155bb80dffb8f
Tim Gimmel <tim@gimmel.org>
2013-06-02 23:53:18 -0500

    Repaired hash function, will do a basic lookup, however once a call is seen the next instance is overwritten.
      --Tim

--- logbklookup.pl
+++ logbklookup.pl
@@ -5,21 +5,26 @@ use Data::Dumper;
 
 #open IN, "/home/tim/jLog-KY4J/RegularLogbook-KY4J.jdb";
 open my $fh_log, "<", "RegularLogbook-KY4J.jdb";
-my ($line, @logrec, $date, $time, $call, $band, $mode, %log);
+my ($line, $c, @logrec, $date, $time, $call, $band, $mode, %log, $qsoinfo);
 
 while ($line = <$fh_log>) {
     chomp($line);
-    $line =~ s/^\s+//;
+    $line =~ s/^\s+//g;
     @logrec = split(/;/,$line);
     #print Dumper(@logrec);
     ($date, $time, $call, $band, $mode) = @logrec;
-    print "$date $time $call $band $mode \n";
+    #print "$date $time $call $band $mode \n";
     #Load the hash
-    %log= {
-        $call => "$date $time $band $mode"
-    }   
-    
+    $qsoinfo = "$date $time $band $mode";
+    $log{ $call } = $qsoinfo;
+    $c++;
 }
+print "Loaded $c records.\n";
 
-print "$log{'N9JCA'}\n";
+#my ($key, $value);
+#while (($key, $value) = each %log) {
+#    print "$key $value\n";
+#}
+#print Dumper(%log);
+print $log{'KY4K'} . "\n";
 
